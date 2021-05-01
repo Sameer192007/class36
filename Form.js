5da1c93d5ade9bae929950e8e15ae98f80f26a39
Sameer192007 <75990855+Sameer192007@users.noreply.github.com>
2021-05-01 19:08:26 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,29 @@
+class Form{
+    constructor(){ }
+
+    display(){
+        var title=createElement('h2');
+        title.html("Car Racing Game");
+        title.position(380,0);
+
+        var input=createInput("Name");
+        var button=createButton("Play");
+        var greeting=createElement('h3');
+
+        input.position(380,160);
+        button.position(420,250);
+
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+
+            var name=input.value();
+            playerCount=playerCount+1;
+            player.update(name);
+            player.updateCount(playerCount);
+            greeting.html("Hello"+name);
+            greeting.position(140,160);
+
+        })
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,15 @@
+var gameState=0;
+var playerCount;
+var form, player, game;
+
+function setup(){
+  database = firebase.database();
+  game=new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+}
+
